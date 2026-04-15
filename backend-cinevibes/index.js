bd39b5f53edf37d955ef4e7add0da5b1f935089e
idarshanapatil <d_patil@u.pacific.edu>
2026-04-15 16:16:12 -0700

    Update index.js

--- backend-cinevibes/index.js
+++ backend-cinevibes/index.js
@@ -7,9 +7,7 @@ const movieRoutes = require("./routes/movie");
 const app = express();
 
 
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.static('dist'));
 
