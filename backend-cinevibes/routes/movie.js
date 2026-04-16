4ca69faec547cd6d353939cf825738b7ea0ff8a9
Khoa Tran <dangkhoa2401bg@gmail.com>
2026-04-16 02:33:13 -0700

    npm_generated_rebuild_of_the_UI

--- backend-cinevibes/routes/movie.js
+++ backend-cinevibes/routes/movie.js
@@ -5,5 +5,6 @@ const movieController = require("../controllers/movie");
 router.get("/trending", movieController.getTrending);
 router.get("/:id", movieController.getMovieDetail);
 router.post("/comments", movieController.addComment);
+router.get("/:id/comments", movieController.getComments);
 
 module.exports = router;
