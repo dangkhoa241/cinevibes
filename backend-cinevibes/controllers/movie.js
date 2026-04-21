10dc5ee7eb103b2101c2cce5fdbc9b2e3c2ccec1
Khoa Tran <dangkhoa2401bg@gmail.com>
2026-04-21 00:05:20 -0700

    merge

--- backend-cinevibes/controllers/movie.js
+++ backend-cinevibes/controllers/movie.js
@@ -3,10 +3,17 @@ const Movie = require("../models/movie");
 // GET /api/movies/trending - For Rankings
 exports.getTrending = async (req, res) => {
     try {
+        const page= parseInt(req.query.page) || 1;
+        const limit= 10;
+        const skip= (page - 1) * limit;
+        const totalMovies = await Movie.countDocuments();
+
         const trending = await Movie.find()
-            .sort({ discussionCount: -1 }) // Based on user comments [cite: 31]
-            .limit(10);
-        res.json(trending);
+            .sort({ discussionCount: -1, _id:1 }) // Based on user comments [cite: 31]
+            .skip(skip)
+            .limit(limit);
+
+        res.json({movies: trending, totalMovies, page, limit});
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
--- frontend-cinevibes/src/pages/Home.jsx
+++ frontend-cinevibes/src/pages/Home.jsx
@@ -3,30 +3,53 @@ import axios from 'axios';
 import MovieCard from '../components/MovieCard';
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
+import Pagination from "../components/Pagination.jsx";
 
-const baseUrl = '/api/movies/trending';
+
 
 const Home = () => {
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [ totalPages, setTotalPages] = useState(0);
+    const [search, setSearch] = useState('');
+    const baseUrl = `/api/movies/trending?page=${page}`;
+
+
 
     useEffect(() => {
         axios.get(baseUrl)
             .then(response => {
-                setMovies(response.data);
+                window .scrollTo({top:0, behaviour: 'smooth'});
+                setMovies(response.data.movies);
+                setTotalPages(Math.ceil(response.data.totalMovies / response.data.limit));
                 setLoading(false);
             })
             .catch(error => {
                 console.error("Error fetching movies:", error);
                 setLoading(false);
             });
-    }, []);
+    }, [page]);
+
+    useEffect(() => {
+        if (search === '') return;  // if empty, don't search
+
+        axios.get(`/api/movies/search?title=${search}`)
+            .then(response => {
+                setMovies(response.data);
+            })
+            .catch(error => {
+                console.error("Search error:", error);
+            });
+    }, [search]);
 
     if (loading) return <div>Loading trending movies...</div>;
 
     return (
-        <>
-            <Header></Header>
+        <div>
+            <Header>
+            </Header>
+            <input type="text" placeholder="Search movies..." value={search} onChange={(e) => setSearch(e.target.value)} />
             <div style={styles.container}>
                 <h1>CineVibes</h1>
                 <div style={styles.grid}>
@@ -34,9 +57,10 @@ const Home = () => {
                         <MovieCard key={movie.imdbID} movie={movie} />
                     ))}
                 </div>
+                <Pagination page={page} setPage={setPage} totalPages={totalPages}></Pagination>
             </div>
             <Footer></Footer>
-        </>
+        </div>
     );
 };
 
